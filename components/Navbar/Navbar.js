a3bc8be2520223414aff3163aaeac4b7b250ba69
Belousov <wasik787@gmail.com>
2022-02-23 13:10:58 +0500

    seo team and navlink

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,3 +1,5 @@
+import { MusicNoteIcon, UsersIcon } from "@heroicons/react/solid";
+
 const Navbar = () => {
   return (
     <div className="drawer fixed w-full z-40">
@@ -9,25 +11,17 @@ const Navbar = () => {
           </div>
           <div className="flex-none hidden px-2 mx-2 lg:flex">
             <div className="flex items-stretch">
-              <a className="btn btn-ghost btn-sm rounded-btn">Likes</a>
-              <a className="btn btn-ghost btn-sm rounded-btn">Notifications</a>
-              <a className="btn btn-ghost btn-sm rounded-btn">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-5 mr-2 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                  ></path>
-                </svg>
-                Files
+              <a className="btn btn-ghost btn-sm rounded-btn" href="#about">
+                О нас
               </a>
-              <a className="btn btn-ghost btn-sm rounded-btn">Config</a>
+              <a className="btn btn-ghost btn-sm rounded-btn" href="#song">
+                <MusicNoteIcon className="h-5 w-5 mr-1" />
+                Песня
+              </a>
+              <a className="btn btn-ghost btn-sm rounded-btn" href="#team">
+                <UsersIcon className="h-5 w-5 mr-1" />
+                Команда
+              </a>
             </div>
           </div>
           <div className="flex-none">
@@ -53,10 +47,21 @@ const Navbar = () => {
         <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
         <ul className="p-4 overflow-y-auto menu w-80 bg-base-100">
           <li>
-            <a>Item 1</a>
+            <a className="" href="#about">
+              О нас
+            </a>
           </li>
           <li>
-            <a>Item 2</a>
+            <a className="" href="#song">
+              <MusicNoteIcon className="h-5 w-5 mr-1" />
+              Песня
+            </a>
+          </li>
+          <li>
+            <a className="" href="#team">
+              <UsersIcon className="h-5 w-5 mr-1" />
+              Команда
+            </a>
           </li>
         </ul>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,40 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Здравствуй, город на Исети</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Город на Исети - Песня к 300-летию Екатеринбурга</title>
+        <meta property="og:title" content="Город на Исети - Песня к 300-летию Екатеринбурга" />
+        {/* <meta property="og:audio" content={SEO.mp3Free} />} */}
+
+        <meta property="og:type" content="website" />
+
+        <meta
+          name="description"
+          content="Музыкальный проект к 300-летию Екатеринбурга, направленный на создание привлекательного образа города, и укрепление межнационального согласия"
+        />
+
+        <meta
+          property="og:description"
+          content={
+            "Музыкальный проект к 300-летию Екатеринбурга, направленный на создание привлекательного образа города, и укрепление межнационального согласия"
+          }
+        />
+
+        <meta
+          name="Keywords"
+          content="Город на Исети 300-летие Екатеринбурга Екатеринбургу 300 лет неофициальный гимн Екатеринбурга Песня к 300-летию Екатеринбурга здравствуй город на Исети Песня про Екатеринбург"
+        ></meta>
+
+        <meta property="og:url" content="https://city-on-the-iset-river.netlify.app" />
+        <meta
+          property="og:image"
+          content="https://city-on-the-iset-river.netlify.app/_ipx/w_640,q_75/%2Fheader2.jpg?url=%2Fheader2.jpg&w=640&q=75"
+        />
       </Head>
 
       <div className={cn("hero", "min-h-screen")}>
         <div className={styles.bg}>
-          <Image layout="fill" src="/header2.jpg" priority />
+          <Image layout="fill" src="/header2.jpg" priority alt="Екатеринбург" />
         </div>
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="text-center hero-content text-neutral-content">
@@ -25,14 +53,14 @@ export default function Home() {
               Здравствуй, <br /> город на Исети!
             </h1>
             <p className="mb-5">Песня к 300-летию Екатеринбурга</p>
-            <button className="btn btn-primary">
+            <a className="btn btn-primary" href="#about">
               <ArrowDownIcon className=" h-5 w-5" />
-            </button>
+            </a>
           </div>
         </div>
       </div>
 
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-screen bg-base-200" id="about">
         <div className="flex-col hero-content lg:flex-row-reverse">
           <div className="flex-shrink-0">
             <Image
@@ -40,6 +68,7 @@ export default function Home() {
               width={600}
               height={600}
               className="max-w-sm rounded-lg shadow-2xl"
+              alt="Автор Песни - Ольга Белоусова"
             />
           </div>
           <div className="prose">
@@ -59,8 +88,84 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="container">
-        <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+      <div className="py-10" id="song">
+        <div className="text-center mb-5">
+          <h2 className=" text-5xl font-bold mx-auto">Город на Исети</h2>
+        </div>
+        <div className="flex flex-wrap relative">
+          <div className="grow min-w-fit md:w-3/5">
+            <div className="card-body sticky top-10">
+              <iframe
+                className="w-full aspect-video rounded-lg shadow-lg h-auto"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/zKYeqLKLXVU"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+          <div className="flex-auto md:w-2/5">
+            <div className="prose card-body">
+              <p>
+                Высотки смотрят вверх, <br />
+                Меряясь друг с другом этажами, <br />
+                И яблоневый снег <br />
+                Кружится над гранитной мостовой. <br />
+                И непонятно мне, <br />
+                Как же не заметили мы сами, <br />
+                Как за двадцать лет
+                <br />
+                Выросли с тобой!
+              </p>
+
+              <p>
+                <i>Припев: </i>
+              </p>
+              <p>
+                Здравствуй, город на Исети! <br />
+                Я в твои попался сети, <br />
+                И в дороге снятся знакомые огни.
+                <br />
+                Этот город на Исети -<br />
+                Здесь посчастливилось мне встретить
+                <br />
+                Самоцветные глаза твои!
+              </p>
+
+              <p>
+                Так много их, столиц, <br />
+                Которые тебя ещё не знают, <br />
+                И столько городов, <br />
+                Что, может быть, давно уже не ждут. <br />
+                Но среди тысяч лиц <br />
+                Одно в улыбке заставляет таять, <br />
+                И к набережной вновь <br />
+                На встречу я иду.
+              </p>
+
+              <p>
+                <i>Припев. </i>
+              </p>
+
+              <p>
+                Не потому ли тут <br />
+                Мечтать и танцевать, как в детстве, просто, <br />
+                Что рядом есть глаза – <br />
+                Глаза твои родные, а вокруг <br />
+                Все те, кто повзрослел, <br />
+                Те, кто вырос вместе со Свердловском, <br />И те, с кем подрастёт Екатеринбург!
+              </p>
+
+              <p>
+                <i>Припев.</i>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
           <div className="card-body">
             <iframe
               className="w-full aspect-video rounded-lg shadow-lg h-auto"
@@ -77,7 +182,6 @@ export default function Home() {
 
         <div className="">
           <div className="py-6 prose">
-            <h2 className="">Город на Исети</h2>
             <p>
               Высотки смотрят вверх, <br />
               Меряясь друг с другом этажами, <br />
@@ -132,7 +236,144 @@ export default function Home() {
               <i>Припев.</i>
             </p>
           </div>
+        </div> */}
+      </div>
+
+      <div className="container py-10" id="team">
+        <div className="text-center mb-5">
+          <h2 className=" text-5xl font-bold mx-auto">Наша команда</h2>
         </div>
+
+        <div className="flex flex-wrap justify-center py-10  gap-10 text-center">
+          <div className="flex flex-col w-56">
+            <div className="avatar mx-auto mb-2">
+              <div className="mask mask-hexagon">
+                <Image
+                  src="/team_1.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full object-top"
+                  alt="Ольга Белоусова"
+                />
+              </div>
+            </div>
+            <div className="prose">
+              <p>
+                <strong>Ольга Белоусова</strong> <br /> <i>слова</i>
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col w-56">
+            <div className="avatar mx-auto mb-2">
+              <div className="mask mask-hexagon">
+                <Image
+                  src="/team_2.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full object-top"
+                />
+              </div>
+            </div>
+            <div className="prose">
+              <p>
+                <strong>Катя Ред</strong> <br /> <i>исполнение на английском языке</i>
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-56">
+            <div className="avatar mx-auto mb-2">
+              <div className="mask mask-hexagon">
+                <Image
+                  src="/team_3.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full object-top"
+                />
+              </div>
+            </div>
+            <div className="prose">
+              <p>
+                <strong>Мария Плотникова</strong> <br /> <i>перевод на французский язык</i>
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col w-56">
+            <div className="avatar mx-auto mb-2">
+              <div className="mask mask-hexagon">
+                <Image
+                  src="/team_4.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full object-top"
+                />
+              </div>
+            </div>
+            <div className="prose">
+              <p>
+                <strong>Михаил Мостов</strong> <br />{" "}
+                <i>исполнение на русском и французском языке</i>
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col w-56">
+            <div className="avatar mx-auto mb-2">
+              <div className="mask mask-hexagon">
+                <Image
+                  src="/team_5.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full object-top"
+                />
+              </div>
+            </div>
+            <div className="prose">
+              <p>
+                <strong>Иван Осенков</strong> <br />{" "}
+                <i>перевод на английский язык, исполнение на английском языке</i>
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-56">
+            <div className="avatar mx-auto mb-2">
+              <div className="mask mask-hexagon">
+                <Image
+                  src="/team_6.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full object-top"
+                />
+              </div>
+            </div>
+            <div className="prose">
+              <p>
+                <strong>Виктор Солдатов</strong> <br /> <i>музыка</i>
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-56">
+            <div className="avatar mx-auto mb-2">
+              <div className="mask mask-hexagon">
+                <Image
+                  src="/team_7.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full object-top"
+                />
+              </div>
+            </div>
+            <div className="prose">
+              <p>
+                <strong>Ли Илунь</strong> <br />{" "}
+                <i>перевод на китайский язык, исполнение на китайском языке</i>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <Image src="/line.svg" width={960} height={100} className="mx-auto" />
       </div>
     </Layout>
   );
